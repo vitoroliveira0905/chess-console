e27b2a8b29906795a396b6c67d77b07be5237f81
Vitor Oliveira <vitor.oliveira2005@hotmail.com.br>
2023-11-27 12:30:08 -0300

    Handling errors with custom exception

--- chess-console/Board/Entities/Board.cs
+++ chess-console/Board/Entities/Board.cs
@@ -1,4 +1,6 @@
-﻿namespace chess_console.board.entities
+﻿using chess_console.board.entities.exceptions;
+
+namespace chess_console.board.entities
 {
     internal class Board
     {
@@ -16,10 +18,38 @@
         {
             return Pieces[rank, file];
         }
+        public Piece Piece(Position position)
+        {
+            return Pieces[position.Rank, position.File];
+        }
+        public bool ThereIsPiece(Position position)
+        {
+            ValidatePosition(position);
+            return Piece(position) != null;
+        }
         public void PlacePiece(Piece piece, Position position)
         {
+            if (ThereIsPiece(position))
+            {
+                throw new BoardException("There is already a piece in that position!");
+            }
             Pieces[position.Rank, position.File] = piece;
             piece.Position = position;
+        }
+        public bool ValidPosition(Position position)
+        {
+            if(position.Rank < 0 || position.Rank >= Rank || position.File < 0 || position.File >= File)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidatePosition(Position position)
+        {
+            if(!ValidPosition(position))
+            {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
---
+++ chess-console/Board/Entities/exceptions/BoardException.cs
@@ -0,0 +1,9 @@
+﻿namespace chess_console.board.entities.exceptions
+{
+    internal class BoardException : Exception
+    {
+        public BoardException(string msg) : base(msg)
+        { 
+        }
+    }
+}
